6dbd657376fbee2939557e606fb420858c56b4b9
Maxime Lust <maxime@favoreatdesign.com>
2026-06-17 10:17:21 +0200

    add hero homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,8 @@
-<h1>Metal access 2</h1>
+<section
+	class="h-lvh w-full flex items-end bg-cover bg-center px-18.75 py-20"
+	style="background-image: linear-gradient(rgba(0,0,0,0.3), rgba(0,0,0,0.3)), url('/src/lib/hero.png');"
+>
+	<h1 class="relative font-fledora text-theme-white text-6.5xl! max-w-2/3">
+		Lorem ipsum dolor sit amet consectetur.
+	</h1>
+</section>
--- src/routes/bonnes-pratiques/articles/+page.svelte
+++ src/routes/bonnes-pratiques/articles/+page.svelte
@@ -1,1 +1,12 @@
+<script lang="ts">
+	let { data } = $props();
+	console.log('data:', data);
+</script>
+
 <h2>Articles et bonnes pratiques</h2>
+
+{#each data.allPages as page (page.id)}
+	<div>
+		<p>{page.properties.Title.title[0].plain_text}</p>
+	</div>
+{/each}
