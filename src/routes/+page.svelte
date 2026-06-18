de0d08d9dfa2fa7750f0d449f3f8a70b79bd2d16
Maxime Lust <maxime@favoreatdesign.com>
2026-06-18 10:42:33 +0200

    remove comments

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,8 +4,6 @@
 	import BonnesPratiques from '$lib/components/Homepage/BonnesPratiques.svelte';
 </script>
 
-<!-- <div class="bg-[url('/bg-home.png')] bg-cover bg-center h-full w-full"> -->
 <Hero />
 <ChiffresCles />
 <BonnesPratiques />
-<!-- </div> -->
